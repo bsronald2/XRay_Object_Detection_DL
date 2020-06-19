2ab2df05a2d71ce7ccb756306691924bf72ad43a
bsronald2 <ronaldbutron@gmail.com>
2020-06-19 12:47:50 +0100

    Improved GDXRay class to load data

--- src/data/GDXray.py
+++ src/data/GDXray.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from pathlib import Path, PurePath
 import os
 from src.data import data_utils
+from src.config import dim
 from src.data.DataGenerator import DataGenerator
 # Helper libraries
 import numpy as np
@@ -11,26 +12,22 @@ import numpy as np
 class GDXray:
     AUTOTUNE = tf.data.experimental.AUTOTUNE
     DIR = Path('data/raw')
-    IMG_HEIGHT = 256
-    IMG_WIDTH = 256
-    BATCH_SIZE = 32
+    IMG_HEIGHT = dim[0]
+    IMG_WIDTH = dim[1]
+    BATCH_SIZE = 16
     BUFFER_SIZE = 3000
-    tf.random.set_seed(1)
-    np.random.seed(1)
 
-    def __init__(self):
-        logger = logging.getLogger(__name__)
-        logger.info('Init GDXray')
-        logger.info(f'Tensor version {tf.__version__}')
+    def __init__(self, input_dir, train_val_ds=False):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info('Init GDXray')
+        self.train_val_ds = train_val_ds
+        self.input_dir = Path(input_dir)
+        # TODO need to retrieve polygons. The next line is useful for retrieve labels form dir names.
         self.classes_name = [i.name for i in GDXray.DIR.glob("*") if i.name != '.gitkeep']
         # Directories
         self.processed_dir = Path('data/processed/')
-        self.ptest_dir = self.processed_dir / 'test'
-        self.ptrain_dir = self.processed_dir / 'train'
         self.raw_dir = Path(GDXray.DIR)
-        # Logs
-        logger.info(f'# test images:{len(list(self.ptest_dir.glob("*/*.png")))}')
-        logger.info(f'# train images:{len(list(self.ptrain_dir.glob("*/*.png")))}')
+
         # Data Augmentation arguments
         self.data_gen_args = dict(rotation_range=45,
                                   width_shift_range=0.33,
@@ -42,13 +39,17 @@ class GDXray:
                                   )
 
     def load_dataset(self):
-        """Return:
-         Bath tuple Iterators (img, label) of train and val data-sets.
         """
-        train_ds = self.__load_batch(str(self.ptest_dir.absolute() / '*/*.png'))
-        val_ds = self.__load_batch(str(self.ptrain_dir.absolute() / '*/*.png'))
+        Parameters:
+            imgs_dir: a str or Path object type
+            train: if param is true load images & label otherwise just images.
+        Return:
+         A data set Iterator (img, label) or Iterator just imgs
+        """
+        self.logger.info(f'images size:{len(list(self.input_dir.glob("*/*.png")))}')
+        data_set = self.__load_batch(str(self.input_dir.absolute() / '*.png'))
 
-        return train_ds, val_ds
+        return data_set
 
     def __load_batch(self, dir, shuffle_buffer_size=2000):
         # Get datasets of all files matching one or more glob patterns.
@@ -62,10 +63,10 @@ class GDXray:
 
         return prepared_ds
 
-    def pre_process(self, input_dir, output_dir):
+    def pre_process(self, output_dir):
         """ Make..."""
-        p_raw_ds = self.__load_batch(str(input_dir / '*.png'), shuffle_buffer_size=GDXray.BUFFER_SIZE)
-        final_dir = PurePath(str(input_dir)).name
+        p_raw_ds = self.__load_batch(str(self.input_dir / '*.png'), shuffle_buffer_size=GDXray.BUFFER_SIZE)
+        final_dir = PurePath(str(self.input_dir)).name
         data_generator = DataGenerator()
         # data_generator.generate_data_K(p_raw_ds, save_to_dir=output_dir, prefix=final_dir, data_gen=self.data_gen_args)
         data_generator.generate_data_I(ds=p_raw_ds, save_to_dir=output_dir)
@@ -79,7 +80,7 @@ class GDXray:
 
     def __decode_img(self, img):
         # convert the compressed string to a 3D uint8 tensor
-        img = tf.image.decode_png(img, channels=3)
+        img = tf.image.decode_png(img, channels=0)
         # Use `convert_image_dtype` to convert to floats in the [0,1] range.
         img = tf.image.convert_image_dtype(img, tf.float32)
 
@@ -90,9 +91,11 @@ class GDXray:
         """
         map_func that returns a new transformed elements of the Data-set.
         """
-        label = self.__get_label(file_path)
         # load the raw data from the file as a string
         img = tf.io.read_file(file_path)
         img = self.__decode_img(img)
+        if self.train_val_ds:
+            label = self.__get_label(file_path)
+            return img, label
 
-        return img, label
+        return img
